81b627b3915570dd77194814c6d51f033cd18aac
Kartik Singh <kartiksingh3337@gmail.com>
2026-03-06 13:17:58 +0530

    fix ssl config for neon postgres pool

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { PrismaClient } = require('@prisma/client');
+const { Pool } = require("pg");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("@prisma/client");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const adapter = new PrismaPg(pool);
